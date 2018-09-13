74577a32e0151336380383b95bf497730f9d5912
Osajulian <you@example.com>
2018-09-12 21:52:54 -0400

    understanding and using state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import './App.css';
 import Person from './components/Person/person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  }
   render() {
     return (
       
       <div className="App">
-        <Person name='basicboy' age='27'>I'm just a basicboy!</Person>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>I'm just a basicboy!</Person>
       </div>
     );
   }
